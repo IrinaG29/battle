b62fb1969eec4847764867bb15f3bd1657cecb59
Irina <igiosanu29@gmail.com>
2020-12-11 19:17:05 +0000

    All steps + Extracting Logic to the Model

--- app.rb
+++ app.rb
@@ -1,32 +1,32 @@
 require 'sinatra/base'
-require_relative './lib/player.rb'
+require_relative './lib/player'
 
 class Battle < Sinatra::Base
-  set :session_secret, 'super secret'
+  # set :session_secret, 'super secret'
   enable :sessions
 
   get '/' do
     erb :index
   end
 
-  post '/names' do
-     $player_1 = Player.new(params[:player_1_name])
+  post "/names" do
+    $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1.name
+    @player_2 = $player_2.name
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1.name
+    @player_2 = $player_2.name
     erb :attack
   end
 
-  # start the server if ruby file executed directly
+  #start the server if ruby file executed directly
   run! if app_file == $0
 end
